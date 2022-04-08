bc49e8cb309f34c3a42119eaf3fa2ec236871cf4
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-08 23:09:53 +0530

    [Rahmath]Add: Method To Check Equality of Lengths

--- LineComparisionProblems/Program.cs
+++ LineComparisionProblems/Program.cs
@@ -1,4 +1,14 @@
 ﻿using LineComparisionProblems;
 
 LineComparision length = new LineComparision(1, 2, 3, 4);
-length.CalculateLength();
+double line1 = length.CalculateLength();
+string a = line1.ToString();
+LineComparision line = new LineComparision(6, 7, 8 , 9);
+double line2 = line.CalculateLength();
+string b = line2.ToString();
+if (a.Equals(b))
+{
+    Console.WriteLine("Both lines Length are equal");
+}
+else
+    Console.WriteLine("lines are not equal"); 
